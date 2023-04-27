f1a285c66431b7e73f046668231206b0fa95c179
Daniela Akerman <daniela.akerman@mi.unc.edu.ar>
2023-04-27 20:41:35 -0300

    trying deploy

--- back-src/sync.ts
+++
@@ -1,7 +0,0 @@
-// import { sequelize } from "./models/connection";
-// import { User, Pet, Auth, Report } from "./models";
-
-// Pet.sequelize.sync({ alter: true }).then((res) => {
-//   console.log(res);
-//   Pet.findAll();
-// });
